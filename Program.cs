3ecd2f98daeffdd9b7cbdf6195483a15f5d10ddd
my pc <my pc@DESKTOP-SGIKIS6>
2022-02-20 17:59:44 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace ChetuInc
+{
+    class PrimeNoRec
+    {
+        public int PrimeNo(int num, int i)
+        {
+
+            if (i == 1)
+                return 1;
+            else
+            {
+                if (num % i == 0)
+                    return 0;
+                else
+                    return PrimeNo(num, i - 1);
+
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("enter a no. :");
+            n = int.Parse(Console.ReadLine());
+            PrimeNoRec z = new PrimeNoRec();
+            int x = z.PrimeNo(n, n / 2);
+            if (x == 1)
+                Console.WriteLine("it is a prime no.");
+            else
+                Console.WriteLine("not a prime no.");
+
+            Console.ReadLine();
+        }
+    }
+}
